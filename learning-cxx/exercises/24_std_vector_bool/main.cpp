ea3f91e6c904db92380d3cccd9af12f0e0c01095
Jins <3150179336@qq.com>
2024-07-25 22:46:30 +0800

    update cpp

--- learning-cxx/exercises/24_std_vector_bool/main.cpp
+++ learning-cxx/exercises/24_std_vector_bool/main.cpp
@@ -7,10 +7,11 @@
 // TODO: 将下列 `?` 替换为正确的代码
 int main(int argc, char **argv) {
     std::vector<bool> vec(100, 1);// TODO: 正确调用构造函数
+    std::cout << sizeof(vec) << std::endl;
     ASSERT(vec[0], "Make this assertion pass.");
     ASSERT(vec[99], "Make this assertion pass.");
     ASSERT(vec.size() == 100, "Make this assertion pass.");
-    ASSERT(sizeof(vec) == 48, "Fill in the correct value.");
+    ASSERT(sizeof(vec) == 32, "Fill in the correct value.");
     {
         vec[20] = false;
         ASSERT(!vec[20], "Fill in `vec[20]` or `!vec[20]`.");
